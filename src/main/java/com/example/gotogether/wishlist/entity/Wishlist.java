be45a157a217d6155afb77e2a3d915413efa4245
jini5 <lim789789@gmail.com>
2023-03-20 03:53:31 +0900

    feat: Wishlist entity 설계중

---
+++ src/main/java/com/example/gotogether/wishlist/entity/Wishlist.java
@@ -0,0 +1,40 @@
+package com.example.gotogether.wishlist.entity;
+
+import com.example.gotogether.auth.entity.User;
+import com.example.gotogether.product.entity.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wishlist")
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class Wishlist {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Product product;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wishlistId;
+
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+}
