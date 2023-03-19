f81c52a2f5c5f52932374922ab72dfb6316ab50d
jini5 <lim789789@gmail.com>
2023-03-20 03:52:32 +0900

    feat: Category entity 설계중

---
+++ src/main/java/com/example/gotogether/category/entity/Category.java
@@ -0,0 +1,43 @@
+package com.example.gotogether.category.entity;
+
+import com.example.gotogether.product.entity.ProductCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long categoryId;
+
+    @OneToMany(mappedBy = "product_category")
+    private List<ProductCategory> productCategory = new ArrayList<>();
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_parent")
+    private Category parent;
+
+    @Column(name = "category_depth")
+    private Long categoryDepth;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+}
