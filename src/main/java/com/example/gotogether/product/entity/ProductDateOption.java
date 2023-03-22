517ae8bb05be39bac8cce2932582fc352ecf890f
jini5 <lim789789@gmail.com>
2023-03-22 23:32:19 +0900

    feat: ProductDateOption 엔티티 추가

---
+++ src/main/java/com/example/gotogether/product/entity/ProductDateOption.java
@@ -0,0 +1,35 @@
+package com.example.gotogether.product.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "product_date_option")
+public class ProductDateOption {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Id
+    @Column(name="product_date_option_id")
+    private Long ProductDateOptionId;
+
+    @Column(name="start_date")
+    private LocalDate startDate;
+
+    @Column(name="end_date")
+    private LocalDate endDate;
+
+    @Column(name="max_people")
+    private int maxPeople;
+
+    @Column(name="max_single_room")
+    private int maxSingleRoom;
+
+}
