6dab8bcb24e2cd50bab16ab4f3ee62bf715373ae
cutegyuseok <yigyuseok@gmail.com>
2023-03-24 15:00:53 +0900

    docs: swagger notes

--- src/main/java/com/example/gotogether/category/controller/CategoryController.java
+++ src/main/java/com/example/gotogether/category/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.gotogether.category.controller;
 import com.example.gotogether.category.dto.CategoryDTO;
 import com.example.gotogether.category.service.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,8 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
+    @ApiOperation(value = "카테고리 목록 조회", notes = "카테고리 목록을 JSON 형태로 조회.\n" +
+            "code: 200 조회 성공, 204 표시할 내용 없음")
     public ResponseEntity<?> getCategories(){
         return categoryService.selectCate();
     }
