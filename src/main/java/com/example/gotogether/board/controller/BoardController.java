b1f71a3f1d0d38b9e14fe3cd04ae107d70e3f3c7
ykim-yeji <ykim5867@gmail.com>
2023-03-24 17:30:20 +0900

    feat: BoardController 게시글 추가 메소드 구현

--- src/main/java/com/example/gotogether/board/controller/BoardController.java
+++ src/main/java/com/example/gotogether/board/controller/BoardController.java
@@ -1,10 +1,13 @@
 package com.example.gotogether.board.controller;
 
+import com.example.gotogether.auth.dto.UserDTO;
+import com.example.gotogether.board.dto.BoardDTO;
 import com.example.gotogether.board.service.BoardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"게시판 서비스"}, description = "게시글 목록 조회")
@@ -26,5 +29,12 @@ public class BoardController {
     @GetMapping("/detail/{boardId}")
     public ResponseEntity<?> findDetailInfo(@PathVariable Long boardId) {
         return boardService.findDetailInfo(boardId);
+    }
+
+    @ApiOperation(value = "게시글 추가", notes = "게시글을 추가한다.\n +" +
+            "code: 201 추가 성공, 401 없는 사용자 또는 잘못된 요청")
+    @PostMapping
+    public ResponseEntity<?> addPost(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody BoardDTO.BoardAddReqDTO boardAddReqDTO) {
+        return boardService.addPost(userAccessDTO, boardAddReqDTO);
     }
 }
