90ae98064b4c3b213b2e0889b29f6539225ad8f5
ykim-yeji <ykim5867@gmail.com>
2023-03-24 13:55:46 +0900

    feat: PageSizeConfig 게시판 페이지 당 게시글 수 상수 추가

--- src/main/java/com/example/gotogether/global/config/PageSizeConfig.java
+++ src/main/java/com/example/gotogether/global/config/PageSizeConfig.java
@@ -3,5 +3,6 @@ package com.example.gotogether.global.config;
 public class PageSizeConfig {
 
     public static final int User_List_Size = 20;
+    public static final int BOARD_LIST_SIZE = 20;
 
 }
