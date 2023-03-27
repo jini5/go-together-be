c731b6b6b5427b0da539e19f6897e39cbdab75c9
cutegyuseok <yigyuseok@gmail.com>
2023-03-28 08:28:15 +0900

    docs: UserController Swagger readability increased

--- src/main/java/com/example/gotogether/auth/controller/UserController.java
+++ src/main/java/com/example/gotogether/auth/controller/UserController.java
@@ -20,28 +20,28 @@ public class UserController {
 
 
     @GetMapping("/myInfo")
-    @ApiOperation(value = "회원정보 요청 (토큰 O)", notes = "회원정보 요청. 현재 로그인회원의 정보를 반환한다.\n" +
+    @ApiOperation(value = "회원정보 요청 (토큰 O)", notes = "회원정보 요청. 현재 로그인회원의 정보를 반환한다.\n\n" +
             "code: 200 사용자 정보 반환 성공, 400 없는 회원 또는 잘못된 요청")
     public ResponseEntity<?> checkUserInfo(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO) {
         return userService.checkUserInfo(userAccessDTO);
     }
 
     @PatchMapping("/myInfo")
-    @ApiOperation(value = "회원정보 수정 (토큰 O)", notes = "기존 비밀번호가 맞다면 DB에 저장한다.\n" +
+    @ApiOperation(value = "회원정보 수정 (토큰 O)", notes = "기존 비밀번호가 맞다면 DB에 저장한다.\n\n" +
             "code: 200 수정 성공, 400 비밀번호 불일치, 401 없는 사용자 또는 잘못된 요청")
     public ResponseEntity<?> updateUser(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody UserDTO.PatchUserReqDTO patchUserReqDTO) {
         return userService.updateUser(userAccessDTO, patchUserReqDTO);
     }
 
     @DeleteMapping("/withdraw")
-    @ApiOperation(value = "회원탈퇴 버튼 (토큰 O)", notes = "기존 비밀번호가 맞다면 DB의 deleteCheck 값 변경.\n" +
+    @ApiOperation(value = "회원탈퇴 버튼 (토큰 O)", notes = "기존 비밀번호가 맞다면 DB의 deleteCheck 값 변경.\n\n" +
             "code: 200 탈퇴 성공, 401 잘못된 사용자 또는 비밀번호 불일치")
     public ResponseEntity<?> deleteUser(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody UserDTO.DeleteUserReqDTO deleteUserReqDTO) {
         return userService.deleteUser(userAccessDTO, deleteUserReqDTO);
     }
 
     @PatchMapping("/type")
-    @ApiOperation(value = "사용자 여행 유형 저장 또는 수정", notes = "기존 유형이 없다면 저장, 있다면 수정.\n" +
+    @ApiOperation(value = "사용자 여행 유형 저장 또는 수정", notes = "기존 유형이 없다면 저장, 있다면 수정.\n\n" +
             "code: 200 저장됨, 404 잘못된 사용자")
     public ResponseEntity<?> saveUserType(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody UserDTO.UserType userType) {
         return userService.saveUserType(userAccessDTO, userType);
