37d19629c8f7c6a0cd287e14344ce7e8d25ac09a
jini5 <lim789789@gmail.com>
2023-03-28 02:28:56 +0900

    feat: ProductOptionRepository 기본 구조 작성

--- src/main/java/com/example/gotogether/product/repository/ProductOptionRepository.java
+++ src/main/java/com/example/gotogether/product/repository/ProductOptionRepository.java
@@ -3,5 +3,12 @@ package com.example.gotogether.product.repository;
 import com.example.gotogether.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+
+    Optional<ProductOption> findByProductOptionId(Long ProductOptionId);
+
+    Optional<ProductOption> deleteAllBy(Long ProductOptionId);
+
 }
