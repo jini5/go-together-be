098830b0b3bcdc0a7857d0054ad5065b8dea6ce6
cutegyuseok <yigyuseok@gmail.com>
2023-03-28 08:30:32 +0900

    docs: PageContentsAdminController Swagger readability increased

--- src/main/java/com/example/gotogether/page/controller/PageContentsAdminController.java
+++ src/main/java/com/example/gotogether/page/controller/PageContentsAdminController.java
@@ -18,21 +18,21 @@ public class PageContentsAdminController {
     private final PageContentsService pageContentsService;
 
     @PostMapping("/regions")
-    @ApiOperation(value = "인기 지역 추가", notes = "관리자가 인기 여행지역 추가. rate 는 노출 단계(낮을수록 먼저 노출) \n" +
+    @ApiOperation(value = "인기 지역 추가", notes = "관리자가 인기 여행지역 추가. rate 는 노출 단계(낮을수록 먼저 노출) \n\n" +
             "code: 201 추가 성공, 400 지역 이름 중복")
     public ResponseEntity<?> addRegion(@RequestBody RegionDTO.RegionReqDTO dto){
         return pageContentsService.addRegion(dto);
     }
 
     @PutMapping("/regions/{regionId}")
-    @ApiOperation(value = "인기 지역 수정", notes = "관리자가 인기 여행지역 수정.\n" +
+    @ApiOperation(value = "인기 지역 수정", notes = "관리자가 인기 여행지역 수정.\n\n" +
             "code: 200 수정 성공, 400 지역 이름 중복 또는 잘못된 ID")
     public ResponseEntity<?> updateRegion(@PathVariable Long regionId,@RequestBody RegionDTO.RegionUpdateReqDTO dto){
         return pageContentsService.updateRegion(regionId,dto);
     }
 
     @DeleteMapping("/regions/{regionId}")
-    @ApiOperation(value = "인기 지역 삭제", notes = "관리자가 인기 여행지역 삭제.\n" +
+    @ApiOperation(value = "인기 지역 삭제", notes = "관리자가 인기 여행지역 삭제.\n\n" +
             "code: 200 삭제 성공, 400 잘못된 ID")
     public ResponseEntity<?> deleteRegion(@PathVariable Long regionId){
         return pageContentsService.deleteRegion(regionId);
