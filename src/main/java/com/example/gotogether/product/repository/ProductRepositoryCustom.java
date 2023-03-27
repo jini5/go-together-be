b9151c955a4d22102288ae302630c9c74717a22f
cutegyuseok <yigyuseok@gmail.com>
2023-03-28 07:58:42 +0900

    feat:  상품 검색 시 날짜와 인원수 조건 추가 repository interface

--- src/main/java/com/example/gotogether/product/repository/ProductRepositoryCustom.java
+++ src/main/java/com/example/gotogether/product/repository/ProductRepositoryCustom.java
@@ -5,9 +5,10 @@ import com.example.gotogether.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProductRepositoryCustom {
-    Page<Product> searchByKeywordAndSorting(Pageable pageable, String keyword, String sort);
+    Page<Product> searchByKeywordAndSorting(Pageable pageable, String keyword, String sort, LocalDate localDate, int people);
     List<Product> findPopular(Category category);
 }
