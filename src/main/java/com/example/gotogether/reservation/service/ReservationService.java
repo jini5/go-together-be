e3e063b605eaf09b4aa7d9987ccf24bcfc8b2a18
ykim-yeji <ykim5867@gmail.com>
2023-03-27 18:59:39 +0900

    feat: ReservationService 예약 상태 수정 추상메소드 추가

--- src/main/java/com/example/gotogether/reservation/service/ReservationService.java
+++ src/main/java/com/example/gotogether/reservation/service/ReservationService.java
@@ -1,8 +1,11 @@
 package com.example.gotogether.reservation.service;
 
+import com.example.gotogether.reservation.entity.ReservationStatus;
 import org.springframework.http.ResponseEntity;
 
 public interface ReservationService {
 
     ResponseEntity<?> findAllList(int pageNumber);
+
+    ResponseEntity<?> modifyReservationStatus(Long reservationId, ReservationStatus reservationStatus);
 }
