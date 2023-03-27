acc0f27304cfcd21a9f847f386bad931655baf63
jini5 <lim789789@gmail.com>
2023-03-28 04:26:37 +0900

    feat: ProductOptionRepository 상품 옵션 수정, 삭제 구현

--- src/main/java/com/example/gotogether/product/repository/ProductOptionRepository.java
+++ src/main/java/com/example/gotogether/product/repository/ProductOptionRepository.java
@@ -3,12 +3,16 @@ package com.example.gotogether.product.repository;
 import com.example.gotogether.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
 
     Optional<ProductOption> findByProductOptionId(Long ProductOptionId);
 
-    Optional<ProductOption> deleteAllBy(Long ProductOptionId);
+    Optional<ProductOption> findAllByProductOptionId(Long ProductOptionId);
+
+    @Transactional
+    Optional<ProductOption> deleteAllByProductOptionId(Long ProductOptionId);
 
 }
