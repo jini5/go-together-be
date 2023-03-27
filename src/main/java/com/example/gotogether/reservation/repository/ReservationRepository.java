428717497e38d3b109dc89d5b8879ec0dd7e746e
ykim-yeji <ykim5867@gmail.com>
2023-03-27 16:27:53 +0900

    feat: ReservationRepository findAll 메소드 구현

---
+++ src/main/java/com/example/gotogether/reservation/repository/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.example.gotogether.reservation.repository;
+
+import com.example.gotogether.reservation.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    Page<Reservation> findAll(Pageable pageable);
+}
