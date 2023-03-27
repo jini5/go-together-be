a65f1bbb716f63498a174bfe33bb1802e0215af9
cutegyuseok <yigyuseok@gmail.com>
2023-03-28 07:45:44 +0900

    feat: Region create Service

---
+++ src/main/java/com/example/gotogether/page/service/PageContentsService.java
@@ -0,0 +1,10 @@
+package com.example.gotogether.page.service;
+
+import com.example.gotogether.page.dto.RegionDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface PageContentsService {
+    ResponseEntity<?> addRegion(RegionDTO.RegionReqDTO dto);
+
+
+}
