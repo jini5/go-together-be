34341ccd7a3a3aaa866de79b83bc6c6bd2a94880
cutegyuseok <yigyuseok@gmail.com>
2023-03-28 08:29:42 +0900

    docs: CategoryController Swagger readability increased

--- src/main/java/com/example/gotogether/category/controller/CategoryController.java
+++ src/main/java/com/example/gotogether/category/controller/CategoryController.java
@@ -17,7 +17,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
-    @ApiOperation(value = "카테고리 목록 조회", notes = "카테고리 목록을 JSON 형태로 조회.\n" +
+    @ApiOperation(value = "카테고리 목록 조회", notes = "카테고리 목록을 JSON 형태로 조회.\n\n" +
             "code: 200 조회 성공, 204 표시할 내용 없음")
     public ResponseEntity<?> getCategories() {
         return categoryService.selectCate();
