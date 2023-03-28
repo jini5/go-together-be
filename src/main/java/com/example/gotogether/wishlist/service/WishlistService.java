776646636eab6404da0c2de567a70645e129e743
jini5 <lim789789@gmail.com>
2023-03-29 03:38:03 +0900

    feat: WishlistService 상품 추가, 삭제 기능

---
+++ src/main/java/com/example/gotogether/wishlist/service/WishlistService.java
@@ -0,0 +1,18 @@
+package com.example.gotogether.wishlist.service;
+import com.example.gotogether.auth.dto.UserDTO;
+import com.example.gotogether.wishlist.dto.WishlistDTO;
+import org.springframework.http.ResponseEntity;
+
+import javax.transaction.Transactional;
+
+
+public interface WishlistService {
+
+
+    @Transactional
+    ResponseEntity<?> createWishlist(UserDTO.UserAccessDTO userAccessDTO, WishlistDTO.WishReqDTO wishReqDTO);
+
+
+    @Transactional
+    ResponseEntity<?> deleteWishlist(UserDTO.UserAccessDTO userAccessDTO,WishlistDTO.DeleteWishReqDTO deletewishReqDTO);
+}
