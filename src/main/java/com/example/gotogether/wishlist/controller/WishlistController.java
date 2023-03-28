ed5195dad2442287b99f2834915ba0dc06546655
jini5 <lim789789@gmail.com>
2023-03-29 03:36:03 +0900

    feat: WishlistController 상품 추가, 삭제 기능

---
+++ src/main/java/com/example/gotogether/wishlist/controller/WishlistController.java
@@ -0,0 +1,43 @@
+package com.example.gotogether.wishlist.controller;
+
+import com.example.gotogether.auth.dto.UserDTO;
+import com.example.gotogether.wishlist.dto.WishlistDTO;
+import com.example.gotogether.wishlist.service.WishlistService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@Api(tags = {"위시리스트 서비스"}, description = "위시리스트 추가, 위시리스트삭제")
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class WishlistController {
+
+
+    private final WishlistService wishlistService;
+
+    @PostMapping("/favor")
+    @ApiOperation(value = "위시리스트 상품 추가", notes = "상품ID를 통해 위시리스트 목록에 상품을 추가한다.\\n\\n" +"code: 200 위시리스트 추가 성공,  code: 404 해당 상품이 이미 있음, code: 500 서버에러")
+    public ResponseEntity<?> addWishlist(UserDTO.UserAccessDTO userAccessDTO, WishlistDTO.WishReqDTO wishReqDTO){
+        return wishlistService.createWishlist(userAccessDTO, wishReqDTO);
+    }
+
+    @DeleteMapping("/favor")
+    @ApiOperation(value = "위시리스트 상품 삭제", notes = "상품ID를 통해 위시리스트 목록에 상품을 추가한다.\\n\\n" +"code: 200 위시리스트 삭제 성공, code: 204 이미 위시리스트에서 삭제됨, code: 500 서버에러")
+    public ResponseEntity<?> deleteWishlist(UserDTO.UserAccessDTO userAccessDTO, WishlistDTO.DeleteWishReqDTO deletewishReqDTO){
+        return wishlistService.deleteWishlist(userAccessDTO,deletewishReqDTO);
+    }
+
+//    @GetMapping("/mypage/favor")
+//    @ApiOperation(value = "내 위시리스트 목록", notes = "회원의 위시리스트 목록을 확인한다.\\n\\n" +"code: 200 위시리스트 삭제 성공, code: 500 서버에러")
+//    public ResponseEntity<?> selectFavorList(){
+//
+//        return wishlistService.
+//    }
+
+}
