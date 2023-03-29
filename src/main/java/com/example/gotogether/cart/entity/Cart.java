a101cdaf52c553ffee83b7951e673249ab8ca765
jini5 <lim789789@gmail.com>
2023-03-29 09:16:00 +0900

    feat: Cart 엔티티 수정

--- src/main/java/com/example/gotogether/cart/entity/Cart.java
+++ src/main/java/com/example/gotogether/cart/entity/Cart.java
@@ -1,9 +1,13 @@
 package com.example.gotogether.cart.entity;
 
 import com.example.gotogether.auth.entity.User;
+import com.example.gotogether.cart.dto.CartDTO;
 import com.example.gotogether.product.entity.Product;
+import com.example.gotogether.product.entity.ProductOption;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -27,6 +31,10 @@ public class Cart {
     @JoinColumn(name = "product_id")
     private Product product;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_option_id")
+    private ProductOption productOption;
+
     @Id
     @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,7 +46,23 @@ public class Cart {
     @Column(name = "single_room_number")
     private int singleRoomNumber;
 
-    @Column(name = "travel_date")
-    private String travelDate;
+
+    @Builder
+    public Cart(User user, Product product, ProductOption productOption, Long cartId, int numberOfPeople, int singleRoomNumber) {
+        this.user = user;
+        this.product = product;
+        this.productOption = productOption;
+        this.cartId = cartId;
+        this.numberOfPeople = numberOfPeople;
+        this.singleRoomNumber = singleRoomNumber;
+    }
+
+    public void update(CartDTO.UpdateCartReqDTO updateCartReqDTO){
+        ProductOption productOption = new ProductOption();
+        productOption.setProductOptionId(updateCartReqDTO.getProductOptionId());
+        this.productOption = productOption;
+        this.numberOfPeople = updateCartReqDTO.getNumberOfPeople();
+        this.singleRoomNumber = updateCartReqDTO.getSingleRoomNumber();
+    }
 
 }
