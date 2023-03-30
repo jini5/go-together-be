76d63728bb08519e269720e9c35d388384de193f
ykim-yeji <ykim5867@gmail.com>
2023-03-30 17:19:12 +0900

    feat: ReservationService 회원 예약 추가 추상메소드 추가

--- src/main/java/com/example/gotogether/reservation/service/ReservationService.java
+++ src/main/java/com/example/gotogether/reservation/service/ReservationService.java
@@ -1,18 +1,20 @@
 package com.example.gotogether.reservation.service;
 
 import com.example.gotogether.auth.dto.UserDTO;
-import com.example.gotogether.reservation.entity.ReservationStatus;
+import com.example.gotogether.reservation.dto.ReservationDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface ReservationService {
 
     ResponseEntity<?> findAllList(int pageNumber);
 
-    ResponseEntity<?> modifyReservationStatus(Long reservationId, ReservationStatus reservationStatus);
+    ResponseEntity<?> modifyReservationStatus(Long reservationId, ReservationDTO.ModifyStatusReqDTO modifyStatusReqDTO);
 
     ResponseEntity<?> findList(UserDTO.UserAccessDTO userAccessDTO, int pageNumber);
 
     ResponseEntity<?> findDetailInfo(UserDTO.UserAccessDTO userAccessDTO, Long reservationId);
 
     ResponseEntity<?> cancelReservation(UserDTO.UserAccessDTO userAccessDTO, Long reservationId);
+
+    ResponseEntity<?> addReservation(UserDTO.UserAccessDTO userAccessDTO, ReservationDTO.AddReqDTO addReqDTO);
 }
