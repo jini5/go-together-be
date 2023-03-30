380b019de6bf88e8952c8bb64eaff2b41fc4ca2d
ykim-yeji <ykim5867@gmail.com>
2023-03-30 17:33:34 +0900

    refactor: ReservationStatus 객체 생성 static 메소드 삭제

--- src/main/java/com/example/gotogether/reservation/entity/ReservationStatus.java
+++ src/main/java/com/example/gotogether/reservation/entity/ReservationStatus.java
@@ -1,7 +1,5 @@
 package com.example.gotogether.reservation.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,9 +9,4 @@ public enum ReservationStatus {
     PENDING("주문 접수"), CONFIRMED("예약 완료"), PAYMENT_PENDING("결제 대기중"), PAYMENT_COMPLETE("결제 완료"), CANCEL_REQUESTED("예약 취소 요청"), CANCELLED("예약 취소"), COMPLETED("여행 완료"), UNDECIDED("미정");
 
     private final String value;
-
-    @JsonCreator
-    public static ReservationStatus from(@JsonProperty("reservationStatus") String value) {
-        return ReservationStatus.valueOf(value);
-    }
 }
