d2087a5d741726d7ee1d304bb53eef4c0683be71
ykim-yeji <ykim5867@gmail.com>
2023-04-01 22:09:39 +0900

    refactor: ReservationStatus 주문 접수 / 결제 완료 / 미정 삭제

--- src/main/java/com/example/gotogether/reservation/entity/ReservationStatus.java
+++ src/main/java/com/example/gotogether/reservation/entity/ReservationStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ReservationStatus {
-    PENDING("주문 접수"), CONFIRMED("예약 완료"), PAYMENT_PENDING("결제 대기중"), PAYMENT_COMPLETE("결제 완료"), CANCEL_REQUESTED("예약 취소 요청"), CANCELLED("예약 취소"), COMPLETED("여행 완료"), UNDECIDED("미정");
+    PAYMENT_PENDING("결제 대기중"), CONFIRMED("예약 완료"), CANCEL_REQUESTED("예약 취소 요청"), CANCELLED("예약 취소"), COMPLETED("여행 완료");
 
     private final String value;
 }
