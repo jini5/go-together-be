69bbe6bc120334fb455aea7ffaea26c0d3502855
ykim-yeji <ykim5867@gmail.com>
2023-04-01 22:11:29 +0900

    refactor: PaymentMethod 미정 삭제 및 한글에서 영어로 변환 메소드 구현

--- src/main/java/com/example/gotogether/reservation/entity/PaymentMethod.java
+++ src/main/java/com/example/gotogether/reservation/entity/PaymentMethod.java
@@ -3,10 +3,23 @@ package com.example.gotogether.reservation.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.NoSuchElementException;
+
 @AllArgsConstructor
 @Getter
 public enum PaymentMethod {
-    NON_BANK_ACCOUNT("무통장 입금"), BANK_TRANSFER("실시간 계좌이체"), UNDECIDED("미정");
+    NON_BANK_ACCOUNT("무통장 입금"), BANK_TRANSFER("실시간 계좌이체");
 
     private final String value;
+
+    public static PaymentMethod from(String method) {
+
+        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+            if (paymentMethod.value.equals(method)) {
+
+                return paymentMethod;
+            }
+        }
+        throw new NoSuchElementException();
+    }
 }
