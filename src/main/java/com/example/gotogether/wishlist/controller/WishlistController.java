02e1d62c50b9ef7df3d92a7443583c66ebb8c255
jini5 <lim789789@gmail.com>
2023-04-03 16:09:15 +0900

    feat: WishlistController URL 수정

--- src/main/java/com/example/gotogether/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/gotogether/wishlist/controller/WishlistController.java
@@ -14,23 +14,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping("/wishlist")
+@RequestMapping
 public class WishlistController {
     private final WishlistService wishlistService;
 
-    @PostMapping
+    @PostMapping("/wishlist")
     @ApiOperation(value = "위시리스트 상품 추가", notes = "상품ID를 통해 위시리스트 목록에 상품을 추가한다. \n\n" +"code: 200 위시리스트 추가 성공,  code: 400 해당 상품이 이미 있음, code: 401 로그인X , code: 500 서버에러")
     public ResponseEntity<?> addWishlist(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody WishlistDTO.WishReqDTO wishReqDTO){
         return wishlistService.createWishlist(userAccessDTO, wishReqDTO);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/wishlist")
     @ApiOperation(value = "위시리스트 상품 삭제", notes = "상품ID를 통해 위시리스트 목록에 상품을 추가한다. \n\n" +"code: 200 위시리스트 삭제 성공, code: 400 이미 위시리스트에서 삭제됨, code: 500 서버에러")
     public ResponseEntity<?> deleteWishlist(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO, @RequestBody WishlistDTO.DeleteWishReqDTO deletewishReqDTO){
         return wishlistService.deleteWishlist(userAccessDTO,deletewishReqDTO);
     }
 
-    @GetMapping("/user")
+    @GetMapping("/user/wishlist")
     @ApiOperation(value = "내 위시리스트 목록", notes = "회원의 위시리스트 목록을 확인한다. \n\n" +"code: 200 위시리스트 삭제 성공, code: 400 위시리스트가 없음, code: 500 서버에러")
     public ResponseEntity<?> findAllWishlist(@AuthenticationPrincipal UserDTO.UserAccessDTO userAccessDTO){
         return wishlistService.findAllWishlistDTO(userAccessDTO);
